f62b9cfadea78728353eadd64a5334294bb1af9d
Ashish Karki <u2aashish@gmail.com>
2019-03-02 07:34:17 -0500

    Initial commit with basic product list component and a json-server.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'product-list' },
+  { path: 'product-list', component: ProductListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
